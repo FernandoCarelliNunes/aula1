6d5bfd1a22f4ff75c071b2238345d55234f9fa9a
FernandoCarelli <49133169+FernandoCarelli@users.noreply.github.com>
2021-08-06 00:12:11 -0300

    operacoes matematicas inteiras

--- aula1/src/aula1/primeiraAula.java
+++ aula1/src/aula1/primeiraAula.java
@@ -2,21 +2,15 @@ package aula1;
 
 public class primeiraAula {
 
-	static int maiorIdadeGlobal = 30; /*variavel globol é acessivel a todos e seu valor é 
-	compartilhado*/
 	
 	public static void main(String[] args) {
-		
-		
-		int maiorIdade = 18; /*variavel local, porque pertence somente a esse metodo*/
-		
-		System.out.println("Valor da variavel local : " + maiorIdade);
-		metodo2();
-		
-	}
 	
-	public static void metodo2() {
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
 		
-		System.out.println("Valor da variavel global é" + maiorIdadeGlobal);
+		System.out.println(" Media das notas = " + (nota1 + nota2 + nota3 + nota4) / 4 );
+		
 	}
 }
