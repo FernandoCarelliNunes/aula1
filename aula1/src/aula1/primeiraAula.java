168abb85dc643087edb9b6965dc109ac2d4a98f4
FernandoCarelli <49133169+FernandoCarelli@users.noreply.github.com>
2021-08-05 23:40:22 -0300

    Adicionando o Projeto

---
+++ aula1/src/aula1/primeiraAula.java
@@ -0,0 +1,23 @@
+package aula1;
+
+public class primeiraAula {
+
+	
+	public static void main(String[] args) {
+		
+		int numero;
+		String nome;
+		
+		
+		numero = 10;
+		nome ="Fernando";
+		
+		System.out.println(numero + "\n" + nome);
+		
+	
+	
+	
+	
+
+}
+}
